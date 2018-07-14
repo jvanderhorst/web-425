62e89160637527576b7f926f48df4dd3188dc607
Johnny Vanderhorst <johnnyrvanderhorst@gmail.com>
2018-07-14 14:41:32 -0400

    Adding week 1 assignments

---
+++ week-1/vanderhorst-hello-modules/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <h1>You have entered the AppComponent</h1>
+    <app-shipping></app-shipping>
+  `,
+  styles: []
+})
+export class AppComponent {
+  title = 'app';
+}
