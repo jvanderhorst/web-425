dbd40753f3d4f97db0faa88ed7a67f902d9db299
Johnny Vanderhorst <johnnyrvanderhorst@gmail.com>
2018-07-30 00:40:27 -0400

    Updating week 3 assignments

--- week-3/vanderhorst-guards/src/app/app.module.ts
+++ week-3/vanderhorst-guards/src/app/app.module.ts
@@ -2,23 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
-import { ProductDetailComponent } from './product-detail/product-detail.component';
-import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import {ReactiveFormsModule} from "@angular/forms";
+import {UnsavedChangesGuard} from "./unsaved-changes.guard";
+import {LoginGuard} from "./login.guard";
+import {HashLocationStrategy, LocationStrategy} from "@angular/common";
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductDetailComponent,
+    HomeComponent,
     LoginComponent,
-    HomeComponent
+    ProductDetailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
   ],
-  providers: [],
+  providers: [ { provide: LocationStrategy, useClass: HashLocationStrategy}, LoginGuard, UnsavedChangesGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
